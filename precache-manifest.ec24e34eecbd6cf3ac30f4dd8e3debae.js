2958ba5d59ffad10e6b4d502471f880cd8c3914a
Navkaran Randhawa <randhawanavkaran@gmail.com>
2020-02-13 15:38:27 -0500

    send it

---
+++ precache-manifest.ec24e34eecbd6cf3ac30f4dd8e3debae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c50d0b57219d19dc86ff9b47048a7f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5069e5ee2594a49e4eee",
+    "url": "./static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8c24b5bc2c698c0db6bc",
+    "url": "./static/css/main.fe58ff47.chunk.css"
+  },
+  {
+    "revision": "5069e5ee2594a49e4eee",
+    "url": "./static/js/2.8951f6de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.8951f6de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c24b5bc2c698c0db6bc",
+    "url": "./static/js/main.08be9b3b.chunk.js"
+  },
+  {
+    "revision": "ebbcfd4fe7d350840879",
+    "url": "./static/js/runtime-main.364db7f1.js"
+  }
+]);
